ac50373545d8adc332f12b9278f089d1dd0ad1cf
Brandon Carr <bmcarr21@me.com>
2020-02-09 14:30:35 -0600

    Finished React Portfolio

--- src/Projects/ProjectObject.js
+++ src/Projects/ProjectObject.js
@@ -5,8 +5,17 @@ const Page =  [
             "alt": "NBA Quiz Screenshot",
             "code": "https://github.com/ThatCoolBlackNerd/NBA-Quiz.git",
             "page": "https://thatcoolblacknerd.github.io/NBA-Quiz/",
-            "skills": "HTML5, CSS, JavaScript, JQuery",
+            "skills": "HTML5, CSS3, JavaScript, JQuery",
             "description":  "Are you a real NBA Fan? Test your basketball knowledge with this Web Quiz using JavaScript and JQuery. This app works on any platform, so you can take it at homeon your computer or on your phone to past time at a restaurant."
+        },
+        {
+            "title": "Comic Book Combat",
+            "screenshot": "/img/cbc_img1.png",
+            "alt": "Character Selection Screen Screenshot",
+            "code": "https://github.com/ThatCoolBlackNerd/Comic-Book-Combat.git",
+            "page": "https://thatcoolblacknerd.github.io/Comic-Book-Combat/",
+            "skills": "HTML5, CSS3, JavaScript, JQuery",
+            "description":  "Super Heroes has become a big part of pop culture and people always argue about who would win in a fight between X character and Y character. This app gives people a reasonable answer to that question."
         }
     ]
 
