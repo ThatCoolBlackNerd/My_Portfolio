8fcaa78f342cdba88fadfa996e0949624278727a
Brandon Carr <bmcarr21@me.com>
2020-02-09 18:11:32 -0600

    Updates

---
+++ precache-manifest.65c2f617b6bb9daea3a348797c375837.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b519954fcc7cef84a563b53024555e",
+    "url": "/My_Portfolio/index.html"
+  },
+  {
+    "revision": "b94d223cacd2c110dbca",
+    "url": "/My_Portfolio/static/css/main.8981c283.chunk.css"
+  },
+  {
+    "revision": "bf207b82509a18aa469a",
+    "url": "/My_Portfolio/static/js/2.c39e4fed.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/My_Portfolio/static/js/2.c39e4fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94d223cacd2c110dbca",
+    "url": "/My_Portfolio/static/js/main.e9c9c690.chunk.js"
+  },
+  {
+    "revision": "4775fa6bed9dcc958bc9",
+    "url": "/My_Portfolio/static/js/runtime-main.b04206d6.js"
+  },
+  {
+    "revision": "5ccaddf836ed7637a5cd413cf89f99c3",
+    "url": "/My_Portfolio/static/media/email.5ccaddf8.png"
+  },
+  {
+    "revision": "819801a0ca3ce6cbc592770922c2fe66",
+    "url": "/My_Portfolio/static/media/github.819801a0.png"
+  },
+  {
+    "revision": "b61b317e4332be6f55b0f59743cedfaf",
+    "url": "/My_Portfolio/static/media/linkedin.b61b317e.png"
+  },
+  {
+    "revision": "75898d0939faacd371036c9f88eba9b5",
+    "url": "/My_Portfolio/static/media/twitter.75898d09.png"
+  }
+]);
